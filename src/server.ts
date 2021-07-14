830c138e797cce6f45e7bf0fbfc0a38b872f0203
Lucas Meyble <lucasmeyble123@gmail.com>
2021-07-14 17:31:15 -0300

    teoria das rotas

--- src/server.ts
+++ src/server.ts
@@ -6,4 +6,12 @@ server.get('/', (req: Request, res: Response) => {
     res.send('alo alo caruaru')
 });
 
+server.get('/contatos', (req: Request, res: Response) => {
+    res.send('contatos')
+});
+
+server.get('/teste', (req: Request, res: Response) => {
+    res.send('teste')
+});
+
 server.listen(3000)
